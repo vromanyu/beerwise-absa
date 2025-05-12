fa67533f556c7420839bebb99c6b28f6d98d70be
vromanyu <vromanyu@netcompany.com>
2025-05-12 11:39:02 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from modules.dataframe_creator.df_creator import generate_processed_dataframe
+import pandas as pd
+
+
+def main():
+    return generate_processed_dataframe()
+
+
+if __name__ == "__main__":
+    final_df = pd.DataFrame()
+    frames = main()
+    for frame in frames:
+        final_df = pd.concat([final_df, frame], ignore_index=True)
+    final_df.reset_index(inplace=True, drop=True)
+    final_df.to_excel("./dataset/sample_dataset_as_excel.xlsx", index=False, engine="openpyxl")
