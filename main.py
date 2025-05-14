007ddfdafe71ead6f753a6b4e1274fa656e43817
vromanyu <vromanyu@netcompany.com>
2025-05-14 09:58:02 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from modules.dataframe_creator.df_creator import generate_processed_dataframe
 import pandas as pd
-import modules.processing.processor as p
-import modules.algorithms.multinomial_nb as m
 
-
-def main():
-    return generate_processed_dataframe()
-
+from modules.dataframe_creator.df_creator import generate_processed_dataframe_chunks
 
 if __name__ == "__main__":
-    final_df = pd.DataFrame()
-    frames = main()
-    for frame in frames:
-        final_df = pd.concat([final_df, frame], ignore_index=True)
-    final_df.reset_index(inplace=True, drop=True)
-    final_df.to_excel("./dataset/sample_dataset_as_excel.xlsx",
-                      index=False, engine="openpyxl")
-    df: pd.DataFrame = m.load_dataframe(
-        f"dataset/sample_dataset_as_excel.xlsx")
+    df = pd.DataFrame()
+    dataframes = generate_processed_dataframe_chunks()
+    for dataframe in dataframes:
+        df = pd.concat([df, dataframe], ignore_index=True)
+    df.reset_index(inplace=True, drop=True)
+    print(df)
