a60859c2a18f12ace29ba7aef53f7d1756c6853f
vromanyu <vromanyu@outlook.com>
2025-05-18 20:54:45 +0300

    generate a dataframe with all rows for now

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-from modules.dataframe_creator.df_creator import generate_processed_dataframe_chunks
+from modules.dataframe_creator.df_creator import generate_processed_dataframe_chunks_with_all_rows
 
 if __name__ == "__main__":
     df = pd.DataFrame()
-    dataframes = generate_processed_dataframe_chunks()
+    dataframes = generate_processed_dataframe_chunks_with_all_rows()
     for dataframe in dataframes:
         df = pd.concat([df, dataframe], ignore_index=True)
     df.reset_index(inplace=True, drop=True)
