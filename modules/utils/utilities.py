1d4393a1740c09d1fb6f353e3f5b9df16614409a
vromanyu <vromanyu@outlook.com>
2025-06-19 10:01:09 +0300

    disable ast

--- modules/utils/utilities.py
+++ modules/utils/utilities.py
@@ -2,8 +2,10 @@ from ast import literal_eval
 
 import pandas as pd
 
+DATASET_EXCEL: str = "dataset/dataset_portion_pre_processed.xlsx"
 
-def load_pre_processed_dataset(dataset: str) -> pd.DataFrame:
+
+def load_pre_processed_dataset(dataset: str = DATASET_EXCEL) -> pd.DataFrame:
     df: pd.DataFrame = pd.read_excel(dataset)
-    df["processed_text"] = df["processed_text"].apply(literal_eval)
+    # df["processed_text"] = df["processed_text"].apply(literal_eval)
     return df
