e537649f1b332e56e9a42f7a7201774b4b7c760a
vromanyu <vromanyu@outlook.com>
2025-08-05 11:33:53 +0300

    add options
    - load preprocessed dataset
    - dump dataset to database
    - load dataset from database

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
 
+from pandas import DataFrame
+
 from modules.processing.processor import normalize_json_dataset, DATASET, create_processed_excel_files
+from modules.utils.utilities import dump_dataframe_to_sqlite, load_dataframe_from_databaes, load_pre_processed_dataset
 
 
 def menu():
-    print("1 - normalize_json_dataset\n2 - create_processed_excel_files")
+    print("1 - normalize_json_dataset\n" +
+          "2 - create_processed_excel_files\n" +
+          "3 - load preprocessed dataset\n" +
+          "4 - dump whole dataset to database\n" +
+          "5 - load dataframe from database\n")
     option: str = input("Enter your option: ")
     if option == "1":
         normalize_json_dataset(DATASET)
@@ -17,6 +24,15 @@ def menu():
         except EOFError:
             sys.exit()
         create_processed_excel_files(limit)
+    elif option == "3":
+        load_pre_processed_dataset()
+    elif option == "4":
+        df: DataFrame = load_pre_processed_dataset()
+        dump_dataframe_to_sqlite(df)
+    elif option == "5":
+        df: DataFrame = load_dataframe_from_databaes()
+        print(df.head())
+        print(df.info())
     else:
         print("invalid option. Exiting...")
 
