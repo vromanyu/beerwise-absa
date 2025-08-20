77731c1c0babbb21d970c1d9f36e5445af88a0a3
vromanyu <vromanyu@outlook.com>
2025-08-20 14:35:40 +0300

    structure saved model location

--- modules/training/fast_text_training.py
+++ modules/training/fast_text_training.py
@@ -1,5 +1,6 @@
 import logging
 import multiprocessing
+import os
 from logging import Logger
 from math import floor
 
@@ -14,8 +15,9 @@ from modules.utils.utilities import (
 
 LOGGER: Logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-MODEL_LOCATION: str = "models/fast_text_for_absa.bin"
-MODEL_SAMPLE_LOCATION: str = "models/fast_text_for_absa_sample.bin"
+MODELS_LOCATION: str = "./models"
+FULL_MODEL_NAME: str = "fast_text_for_absa_full.bin"
+SAMPLE_MODEL_NAME: str = "fast_text_for_absa_sample.bin"
 NUMBER_OF_CORES: int = multiprocessing.cpu_count()
 SIMILARITY_THRESHOLD: float = 0.25
 
@@ -33,22 +35,23 @@ def fast_text_model_trainer(is_sample: bool = False) -> None:
         workers=NUMBER_OF_CORES,
         sg=1,
     )
-    LOGGER.info("dumping model at /models")
+    os.makedirs(f"{MODELS_LOCATION}/fast_text/sample", exist_ok=True)
+    os.makedirs(f"{MODELS_LOCATION}/fast_text/full", exist_ok=True)
     if is_sample:
-        fasttext_model.save(f"{MODEL_SAMPLE_LOCATION}")
+        fasttext_model.save(f"{MODELS_LOCATION}/fast_text/sample/{SAMPLE_MODEL_NAME}")
     else:
-        fasttext_model.save(f"{MODEL_LOCATION}")
+        fasttext_model.save(f"{MODELS_LOCATION}/fast_text/full/{FULL_MODEL_NAME}")
     LOGGER.info("model saved")
 
 
 def load_model(is_sample: bool = False) -> FastText | None:
     try:
         if is_sample:
-            LOGGER.info("loading FastText model")
-            return gensim.models.FastText.load(MODEL_SAMPLE_LOCATION)
-        else:
             LOGGER.info("loading sample FastText model")
-            return gensim.models.FastText.load(MODEL_LOCATION)
+            return gensim.models.FastText.load(f"{MODELS_LOCATION}/fast_text/sample/{SAMPLE_MODEL_NAME}")
+        else:
+            LOGGER.info("loading full FastText model")
+            return gensim.models.FastText.load(f"{MODELS_LOCATION}/fast_text/full/{FULL_MODEL_NAME}")
     except FileNotFoundError:
         LOGGER.error("model was not found or trainer")
         return None
@@ -72,7 +75,8 @@ def generate_similarity_scores_and_labels(is_sample: bool = False) -> None:
 
     for aspect in aspects:
         LOGGER.info(f"assigning sentiment label to aspect: {aspect}")
-        df[f"{aspect}_sentiment"] = df.apply(lambda row: rating_to_sentiment(row[f"{aspect}"]) if row[f"{aspect}_mentioned"] else -2, axis=1)
+        df[f"{aspect}_sentiment"] = df.apply(
+            lambda row: rating_to_sentiment(row[f"{aspect}"]) if row[f"{aspect}_mentioned"] else -2, axis=1)
 
     dump_dataframe_to_sqlite(df, is_sample)
 
@@ -80,18 +84,19 @@ def generate_similarity_scores_and_labels(is_sample: bool = False) -> None:
 def get_similarity(text: list[str], aspect: str, model: FastText):
     return model.wv.n_similarity(text, aspect)
 
+
 def is_aspect_mentioned(row: pd.Series, aspect: str) -> bool:
     return row[f"{aspect}_similarity"] >= SIMILARITY_THRESHOLD
+
 
 def rating_to_sentiment(rating: float) -> int:
     rating = floor(rating)
     if rating >= 4:
-        return 1 # positive label
+        return 1  # positive label
     elif rating == 3:
-        return 0 # neutral label
+        return 0  # neutral label
     else:
         return -1
-
 
 # Unused
 # def generate_and_print_topics():
