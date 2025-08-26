b1abd4c0c9da37a2e5ee6aa45ce8e0f0f7a0f4a1
vromanyu <vromanyu>
2025-08-26 12:06:37 +0300

    add option to start training a transformer-based model

--- main.py
+++ main.py
@@ -3,33 +3,38 @@ import sys
 from pandas import DataFrame
 
 from modules.models.logistic_regression import logistic_regression_trainer
-from modules.models.svm import svm_trainer
+from modules.models.transformer_based import transformer_based_trainer
 from modules.processing.processor import (
     parse_json_dataset,
     DATASET,
     create_processed_excel_files,
 )
-from modules.models.fast_text_training import fast_text_model_trainer, generate_similarity_scores_and_labels
+from modules.models.fast_text_training import (
+    fast_text_model_trainer,
+    generate_similarity_scores_and_labels,
+    find_most_common_aspect_combination,
+)
 from modules.utils.utilities import (
     dump_dataframe_to_sqlite,
     load_dataframe_from_database,
-    load_pre_processed_dataset, create_sample_dataset,
+    load_pre_processed_dataset,
 )
 
 
 def menu():
-    print(""
-          + "1 - parse_json_dataset\n"
-          + "2 - create_processed_excel_files\n"
-          + "3 - load preprocessed dataset\n"
-          + "4 - dump whole dataset to database\n"
-          + "5 - load dataframe from database\n"
-          + "6 - train FastText model\n"
-          + "7 - generate similarity scores and add sentiment labels\n"
-          + "8 - create sample dataset\n"
-          + "9 - train Logistic Regression models (one model per aspect)\n"
-          + "10 - train SVM models (one model per aspect)\n"
-          )
+    print(
+        ""
+        + "1    - parse_json_dataset\n"
+        + "2    - create_processed_excel_files\n"
+        + "3    - load preprocessed dataset\n"
+        + "4    - dump whole dataset to database\n"
+        + "5    - load dataframe from database\n"
+        + "6    - train FastText model\n"
+        + "7    - generate similarity scores and add sentiment labels\n"
+        + "8    - create target dataset by identifying mostly frequently mentioned aspects\n"
+        + "9    - train Logistic Regression model\n"
+        + "10   - train transformer-based model\n"
+    )
     option: str = input("Enter your option: ")
     if option == "1":
         parse_json_dataset(DATASET)
@@ -48,62 +53,20 @@ def menu():
         df: DataFrame = load_pre_processed_dataset()
         dump_dataframe_to_sqlite(df)
     elif option == "5":
-        is_sample: bool = False
-        try:
-            user_input = input("load the sample dataset (y/n): ")
-            if user_input.lower() == "y":
-                is_sample = True
-        except EOFError:
-            sys.exit()
-        df: DataFrame = load_dataframe_from_database(is_sample)
+        df: DataFrame = load_dataframe_from_database()
         print(df.index)
         print(df.info())
         print(df.describe())
     elif option == "6":
-        is_sample: bool = False
-        try:
-            user_input = input("use the sample dataset (y/n): ")
-            if user_input.lower() == "y":
-                is_sample = True
-        except EOFError:
-            sys.exit()
-        fast_text_model_trainer(is_sample)
+        fast_text_model_trainer()
     elif option == "7":
-        is_sample: bool = False
-        try:
-            user_input = input("use the sample dataset (y/n): ")
-            if user_input.lower() == "y":
-                is_sample = True
-        except EOFError:
-            sys.exit()
-        generate_similarity_scores_and_labels(is_sample)
+        generate_similarity_scores_and_labels()
     elif option == "8":
-        length: int = 50_000
-        try:
-            length = int(input("enter sample length (default: 50_000): "))
-        except ValueError:
-            length = 50_000
-        except EOFError:
-            sys.exit()
-        create_sample_dataset(length)
+        find_most_common_aspect_combination()
     elif option == "9":
-        is_sample: bool = False
-        try:
-            user_input = input("use the sample dataset (y/n): ")
-            if user_input.lower() == "y":
-                is_sample = True
-        except EOFError:
-            sys.exit()
-        logistic_regression_trainer(is_sample)
+        logistic_regression_trainer()
     elif option == "10":
-        is_sample: bool = False
-        try:
-            user_input = input("use the sample dataset (y/n): ")
-            if user_input.lower() == "y":
-                is_sample = True
-        except EOFError:
-            sys.exit()
-        svm_trainer(is_sample)
+        transformer_based_trainer()
     else:
         print("invalid option. Exiting...")
         sys.exit()
