1fb5ff3acd88b49ea00060c83a62f2d01e26a1d3
vromanyu <vromanyu>
2025-08-26 11:43:33 +0300

    add helper methods to save and read most common aspects

--- modules/utils/utilities.py
+++ modules/utils/utilities.py
@@ -7,14 +7,15 @@ import os
 
 import pandas as pd
 
-PRE_PROCESSED_PREFIX: str = "dataset_portion_pre_processed"
-DATASET_LOCATION: str = "dataset/"
-DATABASE_URL: str = "sqlite:///dataset.db"
-TARGET_DATABASE_URL: str = "sqlite:///target_dataset.db"
-DATASET_TABLE_NAME: str = "BEER_ADVOCATE"
-TARGET_DATASET_TABLE_NAME: str = "TARGET_BEER_ADVOCATE"
-DATABASE_LOCATION: str = "dataset.db"
-TARGET_DATABASE_LOCATION: str = "target_dataset.db"
+PRE_PROCESSED_PREFIX = "dataset_portion_pre_processed"
+DATASET_LOCATION = "dataset/"
+DATABASE_URL = "sqlite:///dataset.db"
+TARGET_DATABASE_URL = "sqlite:///target_dataset.db"
+DATASET_TABLE_NAME = "BEER_ADVOCATE"
+TARGET_DATASET_TABLE_NAME = "TARGET_BEER_ADVOCATE"
+DATABASE_LOCATION = "dataset.db"
+TARGET_DATABASE_LOCATION = "target_dataset.db"
+ASPECTS_FILE_LOCATION = "most_common_aspects.txt"
 LOGGER: Logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
@@ -28,8 +29,12 @@ def load_pre_processed_dataset() -> pd.DataFrame:
             df: pd.DataFrame = pd.read_excel(f"dataset/{dataset}")
             aggregated_df = pd.concat([aggregated_df, df])
 
-    aggregated_df["processed_text"] = aggregated_df["processed_text"].apply(ast.literal_eval)
-    aggregated_df = aggregated_df[aggregated_df["processed_text"].apply(lambda x: len(x) > 0)]
+    aggregated_df["processed_text"] = aggregated_df["processed_text"].apply(
+        ast.literal_eval
+    )
+    aggregated_df = aggregated_df[
+        aggregated_df["processed_text"].apply(lambda x: len(x) > 0)
+    ]
     return aggregated_df
 
 
@@ -44,18 +49,26 @@ def dump_dataframe_to_sqlite(df: pd.DataFrame, is_target: bool = False) -> None:
     df["processed_text"] = df["processed_text"].apply(str)
     if is_target:
         engine: Engine = create_database_engine(is_target=True)
-        df.to_sql(name=TARGET_DATASET_TABLE_NAME, con=engine, if_exists="replace", index=False)
-        LOGGER.info(f"dataset was dump to sqlite({TARGET_DATABASE_LOCATION}/{TARGET_DATASET_TABLE_NAME})")
+        df.to_sql(
+            name=TARGET_DATASET_TABLE_NAME, con=engine, if_exists="replace", index=False
+        )
+        LOGGER.info(
+            f"dataset was dump to sqlite({TARGET_DATABASE_LOCATION}/{TARGET_DATASET_TABLE_NAME})"
+        )
     else:
         engine = create_database_engine(is_target=False)
         df.to_sql(name=DATASET_TABLE_NAME, con=engine, if_exists="replace", index=False)
-        LOGGER.info(f"dataset was dump to sqlite({DATABASE_LOCATION}/{DATASET_TABLE_NAME})")
+        LOGGER.info(
+            f"dataset was dump to sqlite({DATABASE_LOCATION}/{DATASET_TABLE_NAME})"
+        )
 
 
 def load_dataframe_from_database(is_target: bool = False) -> pd.DataFrame:
     if is_target:
         LOGGER.info(f"loading target dataset from {TARGET_DATABASE_LOCATION}")
-        df: pd.DataFrame = pd.read_sql_table(TARGET_DATASET_TABLE_NAME, TARGET_DATABASE_URL)
+        df: pd.DataFrame = pd.read_sql_table(
+            TARGET_DATASET_TABLE_NAME, TARGET_DATABASE_URL
+        )
         df["processed_text"] = df["processed_text"].apply(ast.literal_eval)
         return df
     else:
@@ -63,3 +76,15 @@ def load_dataframe_from_database(is_target: bool = False) -> pd.DataFrame:
         df: pd.DataFrame = pd.read_sql_table(DATASET_TABLE_NAME, DATABASE_URL)
         df["processed_text"] = df["processed_text"].apply(ast.literal_eval)
         return df
+
+
+def save_most_common_aspects(aspects: list[str]) -> None:
+    with open(ASPECTS_FILE_LOCATION, "w", encoding="utf-8") as f:
+        for aspect in aspects:
+            f.write(f"{aspect}\n")
+
+
+def read_most_common_aspects() -> list[str]:
+    with open(ASPECTS_FILE_LOCATION, "r", encoding="utf-8") as f:
+        aspects = [line.strip() for line in f]
+    return aspects
